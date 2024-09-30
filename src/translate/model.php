3e8a3142d9d8976ba5346af195d7f7caa7932a2e
lafuente-ua <andrey@kevorkyan.info>
2024-09-30 21:00:45 +0300

    Update model.php

--- src/translate/model.php
+++ src/translate/model.php
@@ -7,7 +7,7 @@ class model extends \core\model
 	public const GOOGLE_ALLOWED = true;
 	public const GOOGLE_ALLOWED_AUTO = false;
 	public const GOOGLE_TIMEOUT = 5000;
-	public const GOOGLE_API_KEY = 'AIzaSyA7VrgGr5uh7vrZAfkxJj-Z17h6kvPl-ow';
+	public const GOOGLE_API_KEY = '<YOUR_GOOGLE_API_KEY>';
 	public const GOOGLE_API_URL = 'https://translation.googleapis.com/language/translate/v2';
 
 	protected static $_table_name = 'translate';
